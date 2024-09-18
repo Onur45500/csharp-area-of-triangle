b74be8950ea2628955b2d61bf0cc29f0a75b5d3c
Onur45500 <onur.akmese@viacesi.fr>
2024-09-18 21:20:30 +0200

    Made a simple static void function

--- csharp-area-of-triangle/Program.cs
+++ csharp-area-of-triangle/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace AreaOfTriangle
+﻿using System.Xml.XPath;
+
+namespace AreaOfTriangle
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            string input;
+            Console.WriteLine("Write the base of the triangle :");
+            input = Console.ReadLine();
 
+            double baseTriangle = Convert.ToDouble(input);
+
+            Console.WriteLine("Write the height of the triangle :");
+            input = Console.ReadLine();
+
+            double heightTriangle = Convert.ToDouble(input);
+
+            CalculateTriangleArea(baseTriangle, heightTriangle);
+        }
+
+        static void CalculateTriangleArea(double b, double h)
+        {
+            double result = 0.5 * (b * h);
+            Console.WriteLine($"Triangle area is : {result}");
         }
     }
 }
