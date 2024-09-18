2d7e23299275594403e28f138af7365bfb76b8fd
Onur45500 <onur.akmese@viacesi.fr>
2024-09-18 21:24:37 +0200

    Made a static void function with an out parameter

--- csharp-area-of-triangle/Program.cs
+++ csharp-area-of-triangle/Program.cs
@@ -18,12 +18,21 @@ namespace AreaOfTriangle
             double heightTriangle = Convert.ToDouble(input);
 
             CalculateTriangleArea(baseTriangle, heightTriangle);
+
+            double result = 0;
+            CalculateTriangleAreaWithOut(baseTriangle, heightTriangle, out result);
+            Console.WriteLine($"Triangle area is : {result}");
         }
 
         static void CalculateTriangleArea(double b, double h)
         {
             double result = 0.5 * (b * h);
             Console.WriteLine($"Triangle area is : {result}");
+        }
+
+        static void CalculateTriangleAreaWithOut(double b, double h, out double result)
+        {
+            result = 0.5 * (b * h);
         }
     }
 }
